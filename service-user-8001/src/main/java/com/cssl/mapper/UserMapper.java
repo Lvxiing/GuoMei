96135f6f29cbcaaf594e38500bccd552b58d5980
Lvxiing <1242713963@qq.com>
2019-09-10 15:36:08 +0800

    commit

---
+++ service-user-8001/src/main/java/com/cssl/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.cssl.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cssl.entity.Users;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author lx
+ * @since 2019-09-10
+ */
+public interface UserMapper extends BaseMapper<Users> {
+
+}
