f477204e93f5dee8be130b12b744b4171bd1425d
Lvxiing <1242713963@qq.com>
2019-09-10 15:42:39 +0800

    commit

--- consumer/src/main/java/com/cssl/controller/UserController.java
+++ consumer/src/main/java/com/cssl/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.cssl.controller;
 
 import com.cssl.api.UserFeignInterface;
-import com.cssl.entity.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class UserController {
