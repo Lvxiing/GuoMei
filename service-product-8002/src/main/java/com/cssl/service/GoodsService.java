9f7baca7e9c371e3a1d589042698c5ef82a55a36
Lvxiing <1242713963@qq.com>
2019-09-11 21:17:54 +0800

    更新

--- service-product-8002/src/main/java/com/cssl/service/GoodsService.java
+++ service-product-8002/src/main/java/com/cssl/service/GoodsService.java
@@ -1,8 +1,8 @@
 package com.cssl.service;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cssl.entity.Goods;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.pagehelper.Page;
 
 import java.util.Map;
 
