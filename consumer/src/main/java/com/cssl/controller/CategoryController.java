3857cb71d4a7914bacb416cff0ad034a4daf5445
Lvxiing <1242713963@qq.com>
2019-09-11 21:46:46 +0800

    更新

--- consumer/src/main/java/com/cssl/controller/CategoryController.java
+++ consumer/src/main/java/com/cssl/controller/CategoryController.java
@@ -6,11 +6,13 @@ import com.cssl.entity.Category;
 import com.cssl.entity.TreeCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("category")
@@ -19,6 +21,7 @@ public class CategoryController {
     @Autowired
     private ProductFeignInterface productFeignInterface;
 
+    //--------------------------前台模块-------------------------------
     //查询所有顶级分类
     @RequestMapping("findParentCategory")
     @ResponseBody
@@ -36,5 +39,63 @@ public class CategoryController {
         //将数据进行递归
         return productFeignInterface.findCategoryAndChild(parentId);
     }
+
+
+
+    //--------------------------后台模块-------------------------------
+    @RequestMapping("Goods/findCategory")
+    public String findCategory() {
+        String json = "{\"data\":" + productFeignInterface.findCategory() + "}";
+        return json;
+    }
+
+    @RequestMapping("Goods/updateCategoryInfo/{cid}/{parentId}")
+    public String updateCategoryInfo(@PathVariable("cid") Integer cid, @PathVariable("parentId") Integer parentId) {
+        return productFeignInterface.updateCategoryInfo(cid, parentId);
+    }
+
+    @RequestMapping("Goods/updateCategory")
+    public String updateCategory(@RequestParam Map<String, String> request) {
+        //用于封装数据
+        Category category = new Category();
+        category.setCid(Integer.valueOf(request.get("cid")));
+        category.setName(request.get("categoryName"));
+        category.setParentId(Integer.valueOf(request.get("categoryList")));
+        String json = "{\"res\":" + productFeignInterface.updateCategory(category) + "}";
+        return json;
+
+    }
+
+    @RequestMapping("Goods/deleteCategory/{cid}")
+    public String deleteCategory(@PathVariable("cid") Integer cid) {
+        String json = productFeignInterface.deleteCategory(cid);
+        return json;
+    }
+
+    @RequestMapping("Goods/brandExistGood/{cid}")
+    public String brandExistGood(@PathVariable("cid") Integer cid) {
+        String json = productFeignInterface.brandExistGood(cid);
+        return json;
+    }
+
+    @RequestMapping("Goods/findTreeCategory/{cLevel}")
+    public String findTreeCategory(@PathVariable("cLevel") Integer cLevel) {
+        return productFeignInterface.findTreeCategory(cLevel);
+    }
+
+    @RequestMapping("Goods/addCategory")
+    public String addCategory(@RequestParam Map<String, String> map) {
+        Category category = new Category();
+        category.setName(map.get("parentName") + ":" + map.get("categoryName"));
+        return productFeignInterface.addCategory(category);
+    }
+
+    @RequestMapping("Goods/categoryShow")
+    public List<Category> categoryShow(@RequestParam Map<String, String> param){
+        return productFeignInterface.categoryShow(param);
+    }
+
+
+
 
 }
