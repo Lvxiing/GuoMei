8e02d984da39857738bbaa36b075ab823627eb61
Lvxiing <1242713963@qq.com>
2019-09-11 21:18:40 +0800

    更新

--- service-product-8002/src/main/java/com/cssl/service/impl/CategoryServiceImpl.java
+++ service-product-8002/src/main/java/com/cssl/service/impl/CategoryServiceImpl.java
@@ -4,7 +4,11 @@ import com.cssl.entity.Category;
 import com.cssl.mapper.CategoryMapper;
 import com.cssl.service.CategoryService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -16,5 +20,57 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Override
+    public List<Category> findCategory(Map<String,Object> map) {
+        return categoryMapper.findCategory(map);
+    }
+
+    @Override
+    public Category findCategoryParent(Integer parentId) {
+        return categoryMapper.findCategoryParent(parentId);
+    }
+
+    @Override
+    public int updateCategoryInfo(Category category) {
+        return categoryMapper.updateCategoryInfo(category);
+    }
+
+    @Override
+    public List<Map<String, Object>> findTreeCategory(Integer cLevel) {
+        return categoryMapper.findTreeCategory(cLevel);
+    }
+
+    @Override
+    public List<Map<String, Object>> findCategoryAndParentExist() {
+        return categoryMapper.findCategoryAndParentExist();
+    }
+
+    @Override
+    public int deleteCategory(Integer cid) {
+        return categoryMapper.deleteCategory(cid);
+    }
+
+    @Override
+    public int brandExistGood(Integer cid) {
+        return categoryMapper.brandExistGood(cid);
+    }
+
+    @Override
+    public int deleteBrand(Integer cid) {
+        return categoryMapper.deleteBrand(cid);
+    }
+
+    @Override
+    public int addCategory(Category category) {
+        return categoryMapper.addCategory(category);
+    }
+
+    @Override
+    public int selectBrandId(Integer cid) {
+        return categoryMapper.selectBrandId(cid);
+    }
 
 }
