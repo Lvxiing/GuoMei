5dfc5586c264b8de0fc36128ea7d10f2854783ce
hmn312 <1902732695@qq.com>
2019-09-14 17:04:39 +0800

    规范

--- feign/src/main/java/com/cssl/api/UserFeignInterface.java
+++ feign/src/main/java/com/cssl/api/UserFeignInterface.java
@@ -14,51 +14,51 @@ import java.util.Map;
 public interface UserFeignInterface {
     //使用接口映射
     @RequestMapping("/users/findAll")
-    public List<Users> findAll();
+     List<Users> findAll();
 
     @RequestMapping("/users/ajaxNum")
-    public String sendMsg(@RequestParam("phoneNum") String phoneNum) throws Exception;
+     String sendMsg(@RequestParam("phoneNum") String phoneNum) throws Exception;
 
     @RequestMapping("/users/verfiy")
-    public Map<String,String> login(@RequestParam("phoneNum") String phoneNum, @RequestParam("code") String code);
+     Map<String,String> login(@RequestParam("phoneNum") String phoneNum, @RequestParam("code") String code);
 
     @RequestMapping("/users/selectPhone")
-    public  Users  selectPhone(@RequestParam("phoneNum") String phoneNum);
+      Users  selectPhone(@RequestParam("phoneNum") String phoneNum);
 
     @RequestMapping("/users/userRegister")
-    public   boolean   userRegister(Users users);
+       boolean   userRegister(Users users);
 
     @RequestMapping("/users/selectUserName")
-    public  int  selectUserName(@RequestParam("userName") String userName);
+      int  selectUserName(@RequestParam("userName") String userName);
 
 
     //**********后台***********
 
     @RequestMapping("/users/adminLogin")
-    public Users adminLogin(Users users);
+     Users adminLogin(Users users);
 
     @RequestMapping("/users/findUsers/{userName}/{pageIndex}/{pageSize}")
-    public PageInfo<Users> UsersFenYe(@PathVariable("userName") String userName, @PathVariable("pageIndex") int pageIndex, @PathVariable("pageSize") int pageSize);
+     PageInfo<Users> UsersFenYe(@PathVariable("userName") String userName, @PathVariable("pageIndex") int pageIndex, @PathVariable("pageSize") int pageSize);
 
     @RequestMapping("/users/delUser/{id}")
-    public boolean delUser(@PathVariable("id") Integer id);
+     boolean delUser(@PathVariable("id") Integer id);
 
     @RequestMapping("/users/findById/{id}")
-    public Users findById(@PathVariable("id") Integer id);
+     Users findById(@PathVariable("id") Integer id);
 
     @RequestMapping("/users/updateUser")
-    public boolean updateUser(@RequestParam Map<String,String> map);
+     boolean updateUser(@RequestParam Map<String,String> map);
 
     @RequestMapping("/users/updatePwd")
-    public boolean  updatePwd(Users users);
+     boolean  updatePwd(Users users);
 
     @RequestMapping("/users/selectPwd")
-    public Users selectPwd(Users user);
+     Users selectPwd(Users user);
 
     @RequestMapping("/userinfo/findVip/{pageIndex}/{pageSize}")
-    public PageInfo<Map> UserInfoFenYe(@RequestBody Map map, @PathVariable("pageIndex") int pageIndex, @PathVariable("pageSize") int pageSize);
+     PageInfo<Map> UserInfoFenYe(@RequestBody Map map, @PathVariable("pageIndex") int pageIndex, @PathVariable("pageSize") int pageSize);
 
     @RequestMapping("/grade/allGrade")
-    public List<Grade> allGrade();
+     List<Grade> allGrade();
 
 }
