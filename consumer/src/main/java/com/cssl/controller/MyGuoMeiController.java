c65191b42ad5b639adb7ec4353505c4993d1afe2
Lvxiing <1242713963@qq.com>
2019-09-18 21:00:33 +0800

    更新

--- consumer/src/main/java/com/cssl/controller/MyGuoMeiController.java
+++ consumer/src/main/java/com/cssl/controller/MyGuoMeiController.java
@@ -1,13 +1,28 @@
 package com.cssl.controller;
 
 import com.cssl.api.ProductFeignInterface;
+import com.cssl.entity.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/myGuoMei")
 public class MyGuoMeiController {
+
     @Autowired
     private ProductFeignInterface productFeignInterface;
+
+    @ResponseBody
+    @RequestMapping("/myOrder")
+    public String myOrder(HttpSession session){
+        Users user = (Users)session.getAttribute("user");
+        Integer uid = user.getId();//测试传死值
+        //System.out.println("user = " + user);
+
+        return user.getUserName();
+    }
 }
