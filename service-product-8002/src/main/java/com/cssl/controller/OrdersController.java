4a8214a49fad0e83cf3800396a519d0607a631f2
Lvxiing <1242713963@qq.com>
2019-09-22 02:42:46 +0800

    更新

--- service-product-8002/src/main/java/com/cssl/controller/OrdersController.java
+++ service-product-8002/src/main/java/com/cssl/controller/OrdersController.java
@@ -2,12 +2,16 @@ package com.cssl.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.cssl.entity.Goods;
 import com.cssl.entity.Orders;
 import com.cssl.entity.PageInfo;
+import com.cssl.entity.Users;
 import com.cssl.mapper.Order_detailMapper;
+import com.cssl.service.GoodsService;
 import com.cssl.service.Order_detailService;
 import com.cssl.service.OrdersService;
 import com.github.pagehelper.Page;
+import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +20,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
+import java.rmi.server.UID;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author lx
@@ -31,11 +38,43 @@ import java.util.Map;
 @Controller
 @RequestMapping("/orders")
 public class OrdersController {
+
     @Autowired
     private OrdersService ordersService;
+
     @Autowired
     private Order_detailService orderDetailService;
+
+    @Autowired
+    private GoodsService goodsService;
+
     //-----------------------------前台模块----------------------------
+    //用户下单信息
+    @RequestMapping("orderInfo")
+    @ResponseBody
+    public Map<String, Object> orderInfo(@RequestParam("uid") Integer uid, @RequestParam Map<String, Object> map) {
+        System.out.println("uid**************** = " + uid);
+        Map<String, Object> data = new HashMap<>();
+        List<Map<String, Object>> goodsList = new ArrayList<>();
+        String[] gid = map.get("goodsId").toString().split(",");
+        System.out.println("gid = " + gid);
+        String[] num = map.get("num").toString().split(",");
+        for (int i = 0; i < gid.length; i++) {
+            Goods goods = goodsService.getOne(new QueryWrapper<Goods>().eq("goods_id",gid[i]));
+            Map<String,Object> param = new HashMap<>();
+            param.put("gid",goods.getId());
+            param.put("title",goods.getTitle());
+            param.put("img",goods.getMainImg());
+            double price = goods.getPrice().doubleValue() * Double.valueOf(num[i]);
+            param.put("price",price);
+            param.put("num",num[i]);
+            goodsList.add(param);
+        }
+        data.put("goods",goodsList);
+        return data;
+    }
+
+
     //用户订单
     @RequestMapping("findOrdersByUserId")
     @ResponseBody
@@ -54,113 +93,115 @@ public class OrdersController {
     //根据订单编号查询订单明细
     @RequestMapping("findOrdersDetail")
     @ResponseBody
-    public List<Map<String,Object>> findOrdersDetail(@RequestParam("oid") Integer oid){
+    public List<Map<String, Object>> findOrdersDetail(@RequestParam("oid") Integer oid) {
         return ordersService.findOrdersDetail(oid);
     }
 
     //根据订单编号查询订单信息
     @RequestMapping("findOrders")
     @ResponseBody
-    public Orders findOrders(@RequestParam("oid") Integer oid){
-        return ordersService.getOne(new QueryWrapper<Orders>().eq("order_id",oid));
+    public Orders findOrders(@RequestParam("oid") Integer oid) {
+        return ordersService.getOne(new QueryWrapper<Orders>().eq("order_id", oid));
     }
 
     //查询用户订单的待付款待收货总记录数
     @RequestMapping("findTotal")
     @ResponseBody
-    public List<Map<String,Object>> findTotal(@RequestParam("uid") Integer uid){
+    public List<Map<String, Object>> findTotal(@RequestParam("uid") Integer uid) {
         return ordersService.findTotal(uid);
     }
 
     //根据订单号查询地址表的相关信息
     @RequestMapping("findAddressByOrder")
     @ResponseBody
-    public Map<String,Object> findAddressByOrder(@RequestParam("oid") Integer oid){
+    public Map<String, Object> findAddressByOrder(@RequestParam("oid") Integer oid) {
         return ordersService.findAddressByOrder(oid);
     }
+
     //-----------------------------后台模块----------------------------
-     //查询所有订单详情
-     @RequestMapping("/orderList/{pageIndex}/{pageSize}/{orderNo}/{name}")
-     @ResponseBody
-     public PageInfo<Map<String, Object>> orderList(@PathVariable("pageIndex")Integer pageIndex, @PathVariable("pageSize")Integer pageSize, @PathVariable("orderNo")String orderNo, @PathVariable("name")String name){
-         Map<String,Object> map=new HashMap<>();
-         map.put("orderNo",orderNo);
-         map.put("name",name);
-         Page<Map<String, Object>> maps = ordersService.orderList(map, pageIndex, pageSize);
-         PageInfo<Map<String, Object>>page=new PageInfo<>();
-         List<Map<String, Object>> result = maps.getResult();
-         //封装查询数据
-         page.setList(result);
-         //封装总记录数
-         page.setTotalCount((int)maps.getTotal());
-         return page;
-     }
+    //查询所有订单详情
+    @RequestMapping("/orderList/{pageIndex}/{pageSize}/{orderNo}/{name}")
+    @ResponseBody
+    public PageInfo<Map<String, Object>> orderList(@PathVariable("pageIndex") Integer pageIndex, @PathVariable("pageSize") Integer pageSize, @PathVariable("orderNo") String orderNo, @PathVariable("name") String name) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("orderNo", orderNo);
+        map.put("name", name);
+        Page<Map<String, Object>> maps = ordersService.orderList(map, pageIndex, pageSize);
+        PageInfo<Map<String, Object>> page = new PageInfo<>();
+        List<Map<String, Object>> result = maps.getResult();
+        //封装查询数据
+        page.setList(result);
+        //封装总记录数
+        page.setTotalCount((int) maps.getTotal());
+        return page;
+    }
 
     //修改回显
     @RequestMapping("/ByIdOrders/{orderNo}")
     @ResponseBody
-    public List<Map<String,Object>> ByIdOrders(@PathVariable("orderNo")String orderNo){
+    public List<Map<String, Object>> ByIdOrders(@PathVariable("orderNo") String orderNo) {
         return ordersService.ByIdOrders(orderNo);
     }
 
     //修改订单状态
     @RequestMapping("/updateStatus")
     @ResponseBody
-    public int updateStatus(@RequestParam("orderNo") String orderNo, @RequestParam("status") int status){
-        Orders orders=new Orders();
+    public int updateStatus(@RequestParam("orderNo") String orderNo, @RequestParam("status") int status) {
+        Orders orders = new Orders();
         orders.setStatus(status);
         orders.setOrderNo(orderNo);
-        return  ordersService.updateStatus(orders);
+        return ordersService.updateStatus(orders);
 
     }
 
     //删除订单
     @RequestMapping("/deleteOrders")
     @ResponseBody
-    public int deleteOrders(@RequestParam("orderId")Integer orderId){
+    public int deleteOrders(@RequestParam("orderId") Integer orderId) {
         //先删除订单详情表
         int i = orderDetailService.deletOrderDetail(orderId);
-        int num=0;
-        if(i>0) {
+        int num = 0;
+        if (i > 0) {
             num = ordersService.deleteOrder(orderId);
         }
         return num;
     }
+
     //计算本月退单数量,订单数量和金额,本月订单实际金额,本月未付款金额
     @RequestMapping("/inquiryAmount")
     @ResponseBody
-    public Map<String,Object> inquiryAmount(){
-        return  ordersService.orderQuantity();
+    public Map<String, Object> inquiryAmount() {
+        return ordersService.orderQuantity();
     }
 
     //查询最近一周的订单金额,未付金额,退款金额,实际金额
     @RequestMapping("/weekInquiryAmount")
     @ResponseBody
-    public Map<String,Object> weekInquiryAmount(){
-        return  ordersService.weekOrderQuantity();
+    public Map<String, Object> weekInquiryAmount() {
+        return ordersService.weekOrderQuantity();
     }
+
     //查询最近一周对应日期的订单金额,未付金额,退款金额,实际金额
     @RequestMapping("/weekOrder")
     @ResponseBody
-    public List<Map<String,Object>> weekOrder(){
-        return  ordersService.weekOrder();
+    public List<Map<String, Object>> weekOrder() {
+        return ordersService.weekOrder();
     }
 
 
     //查看订单详情购买商品信息
     @RequestMapping("/orderDetail")
     @ResponseBody
-    public PageInfo<Map<String, Object>> orderDetail(@RequestParam("order_no")String order_no,@RequestParam("page")int page,@RequestParam("limit")int limit){
-        PageInfo<Map<String, Object>>pages=new PageInfo<>();
-        Page<Map<String, Object>> maps = ordersService.orderDetail(order_no,page, limit);
+    public PageInfo<Map<String, Object>> orderDetail(@RequestParam("order_no") String order_no, @RequestParam("page") int page, @RequestParam("limit") int limit) {
+        PageInfo<Map<String, Object>> pages = new PageInfo<>();
+        Page<Map<String, Object>> maps = ordersService.orderDetail(order_no, page, limit);
         List<Map<String, Object>> result = maps.getResult();
         //封装查询数据
         pages.setList(result);
         //封装总记录数
-        pages.setTotalCount((int)maps.getTotal());
-         return pages;
+        pages.setTotalCount((int) maps.getTotal());
+        return pages;
     }
-
 
 
 }
