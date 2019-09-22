4462d1f6fb761bbab86aa259f0d3461f011233e5
LogoLiuJun <2650702166@qq.com>
2019-09-23 02:18:18 +0800

    会员首页添加足迹

--- consumer/src/main/resources/static/js/myindex.js
+++ consumer/src/main/resources/static/js/myindex.js
@@ -3,6 +3,8 @@ $(function () {
     total();
     //查询近三个月的订单
     findOrdersByUserId();
+    //足迹
+    foot();
 });
 
 //查询用户订单的待付款待收货总记录数
@@ -48,7 +50,7 @@ function findOrdersDetail(order_no,order_id) {
         for(var i=0;i<1;i++){
             var dl="<dl class='order-list'> <dt> <a href='product_details.html?gid="+data[i].gid+"' target='_blank'>  <img src='"+data[i].img+"' alt='"+data[i].title+"' width='80' height='80'></a> ";
             dl+="</dt> <dd> <ul> <li>订单编号："+order_no+"</li> <li>共"+data[i].num+"件商品</li><li class='order-money'>¥"+data[i].goods_price+"</li> </ul>   ";
-            dl+="<div class='datails-btn' style='position:relative;'><a href='gm-orederList.html?oid="+data[0].oid+"&ono="+guid()+order_no+"' target='_blank' style='margin-top:10px;'>查看更多订单信息</a> </div> </dd> </dl>";
+            dl+="<div class='datails-btn' style='position:relative;'><a href='gm-orederList.html?oid="+data[0].oid+"&ono="+guid()+order_no+"' target='_blank' style='margin-top:10px;'>查看订单详情</a> </div> </dd> </dl>";
             $(".homeLeft-order").append(dl);
         }
     });
@@ -62,5 +64,17 @@ function noData() {
 
 //足迹
 function foot() {
-    
+    $.getJSON("../../Goods/findBrowseGoods",function (json) {
+        if(json.msg=="success"){
+            var list=json.list;
+            for(var i=0;i<list.length;i++){
+                var li="<li> <div class='myFootprint-img'><a href='product_details.html?gid="+list[i].id+"' target='_blank'><img src='"+list[i].mainImg+"' ></a>";
+                li+="</div> <p class='myFootprint-areaPrice'>¥"+list[i].price+"</p> </li>";
+                $(".myFootprint-box").append(li);
+            }
+        }else{
+            var li="<li>无足迹</li>"
+            $(".myFootprint-box").append(li);
+        }
+    });
 }
