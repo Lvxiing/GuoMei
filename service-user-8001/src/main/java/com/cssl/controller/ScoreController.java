0ad5da0313bc32da2db0f01c8bb60a67acc15759
LogoLiuJun <2650702166@qq.com>
2019-09-25 19:39:57 +0800

    修改

--- service-user-8001/src/main/java/com/cssl/controller/ScoreController.java
+++ service-user-8001/src/main/java/com/cssl/controller/ScoreController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
 
+import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -48,6 +49,7 @@ public class ScoreController {
             scores.setUserId(uid);
             //查询相关积分值
             final  String scoreType_name=map.get("typeName").toString();
+            //判断是否是购物
             map1.put("scoreType_name",scoreType_name);
             Collection<ScoreType> collection = score_typeService.listByMap(map1);
             Iterator iterator = collection.iterator();
@@ -55,7 +57,14 @@ public class ScoreController {
             while(iterator.hasNext()){
                 scoreType=(ScoreType)iterator.next();
             }
-            scores.setScores(scoreType.getValue());
+            if(scoreType_name.equals("购物")){
+                Integer money=new Integer(map.get("money").toString());
+                double score=Math.ceil(money*Integer.valueOf(scoreType.getValue().toString()));
+                scores.setScores(Integer.valueOf(String.valueOf(score)));
+            }else{
+                scores.setScores(Integer.valueOf(scoreType.getValue().toString()));
+            }
+
             return   scoreService.save(scores)?1:0;
         }
 
