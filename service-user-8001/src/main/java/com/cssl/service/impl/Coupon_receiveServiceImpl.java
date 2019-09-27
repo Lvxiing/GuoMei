f8d14363ab25d4383e693e7a664040bee899f270
hmn312 <1902732695@qq.com>
2019-09-27 10:54:34 +0800

    会员信息显示该用户拥有的可使用的优惠券数量

--- service-user-8001/src/main/java/com/cssl/service/impl/Coupon_receiveServiceImpl.java
+++ service-user-8001/src/main/java/com/cssl/service/impl/Coupon_receiveServiceImpl.java
@@ -1,10 +1,13 @@
 package com.cssl.service.impl;
 
 import com.cssl.entity.CouponReceive;
+import com.cssl.mapper.Coupon_logsMapper;
 import com.cssl.mapper.Coupon_receiveMapper;
 import com.cssl.service.Coupon_receiveService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * <p>
@@ -15,6 +18,10 @@ import org.springframework.stereotype.Service;
  * @since 2019-09-10
  */
 @Service
+@Transactional
 public class Coupon_receiveServiceImpl extends ServiceImpl<Coupon_receiveMapper, CouponReceive> implements Coupon_receiveService {
+
+    @Autowired
+    private Coupon_receiveMapper coupon_receiveMapper;
 
 }
